e642dbaf527ce40008f788843f6b18ffaf603a1a
Harry <harryandrew.dix@gmail.com>
2015-09-16 22:07:53 +0100

    new game file, new feature tests

--- lib/another_ships.rb
+++ lib/another_ships.rb
@@ -7,7 +7,14 @@ class Another_ships < Sinatra::Base
   end
 
   get '/new_player' do
+    @name = params[:name]
+    session[:name] = params[:name]
     erb :new_player
+  end
+
+  get '/new_game' do
+    @name = params[:name]
+    erb :new_game
   end
 
   # start the server if ruby file executed directly
--- spec/features/newgame_spec.rb
+++ spec/features/newgame_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 feature 'Starting a new game' do
+  
   scenario 'I am asked to enter my name' do
     visit '/'
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'I enter my name' do
+    visit '/new_player'
+    fill_in 'name', :with => 'harry'
+    click_button "Submit"
+    expect(page).to have_content "Welcome harry!"
+  end
+
 end
