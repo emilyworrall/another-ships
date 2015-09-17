c642af67747efbeb43a603702eb5bff1d5ebde74
Emily Worrall <worrall.emily@gmail.com>
2015-09-17 17:07:06 +0100

    Added a background..

--- lib/another_ships.rb
+++ lib/another_ships.rb
@@ -22,6 +22,30 @@ class Another_ships < Sinatra::Base
     if coords_1 && orient_1
       @board.place(@aircraft_carrier, coords_1, orient_1)
     end
+    @battleship = Ship.battleship
+    coords_2 = params[:coords_2].to_sym if params[:coords_2]
+    orient_2 = params[:orient_2].to_sym if params[:orient_2]
+    if coords_2 && orient_2
+      @board.place(@battleship, coords_2, orient_2)
+    end
+    @destroyer = Ship.destroyer
+    coords_3 = params[:coords_3].to_sym if params[:coords_3]
+    orient_3 = params[:orient_3].to_sym if params[:orient_3]
+    if coords_3 && orient_3
+      @board.place(@destroyer, coords_3, orient_3)
+    end
+    @submarine = Ship.submarine
+    coords_4 = params[:coords_4].to_sym if params[:coords_4]
+    orient_4 = params[:orient_4].to_sym if params[:orient_4]
+    if coords_4 && orient_4
+      @board.place(@submarine, coords_4, orient_4)
+    end
+    @patrol_boat = Ship.patrol_boat
+    coords_5 = params[:coords_5].to_sym if params[:coords_5]
+    orient_5 = params[:orient_5].to_sym if params[:orient_5]
+    if coords_5 && orient_5
+      @board.place(@patrol_boat, coords_5, orient_5)
+    end
 
     erb :new_game
   end
